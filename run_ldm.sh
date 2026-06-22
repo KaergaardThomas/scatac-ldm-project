8c24a3668ecbf066e135f401e1de00c98a527131
BOTAlex <zhentao2004@gmail.com>
2026-06-22 17:51:34 +0200

    Eval multi seed conf int

--- run_ldm.sh
+++ run_ldm.sh
@@ -1,13 +1,13 @@
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 42
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 237
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 284
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 156
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 708
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 844
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 444
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 397
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 431
-uv run python src/train_ldm.py --device 0 1 --epochs 25 --batch_size 8388608 --latent_dim 16 --seeds 437
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 42
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 237
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 284
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 156
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 708
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 844
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 444
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 397
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 431
+uv run python src/train_ldm.py --device 0  --epochs 25 --batch_size 1024000 --latent_dim 16 --seeds 437
 
 uv run python src/plot_ldm.py \
      --base_model_dir results/ldm_model \
@@ -19,3 +19,11 @@ uv run python src/print_results.py \
      --base_model_dir results/ldm_model \
      --base_out_data data/hematopoiesis_with_ldm \
      --latent_dim 2 4 8 16 32 64
+
+uv run python src/evaluate.py \
+    --data data/hematopoiesis_GSE129785_FACS_sorted.h5ad \
+    --ldm_dir results/ldm_model/seed_42_dim_16 \
+    --out_dir results/evaluation
+
+
+42 237 284 156 708 844 444 397 431 437
--- src/zhen/run_peakvi.sh
+++
@@ -1,3 +0,0 @@
-# So i don't forget
-uv run python src/train_peakvi.py --data data/hematopoiesis_GSE129785_FACS_sorted.h5ad --out_data data/hematopoiesis_with_peakvi.h5ad --model_dir results/peakvi_model --devices 1 --epochs 25
-uv run python src/plot_peakvi.py --peakvi_dir results/peakvi_model --out_dir results/peakvi_evaluation
